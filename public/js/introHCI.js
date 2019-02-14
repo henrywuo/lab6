d21388d08cd9af6f0e0c84e0b6225caf61e35aa2
Henry Wu Ou <hwou@ucsd.edu>
2019-02-14 10:44:21 -0800

    lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,34 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	
+	$.get("/project/"+idNumber,addProject);
+	
+	console.log("/project/"+idNumber);
+}
+
+function addProject(result){
+    console.log(result);
+    var id = result['id'];
+    var projectHTML = '<img src ="'+result['image']+'" class = "detailsImage">'+
+        '<p>'+result['title']+'</p>'+
+        '<p><small>'+result['date']+'</small></p>'+
+        '<p>'+result['summary']+'</p>';
+    $("#project"+id+" .details").html(projectHTML);
+}
+
+function randomizeColors(e) {
+	console.log("User clicked on color button");
+    $.get("/palette",changePalette);
+}
+
+function changePalette(result){
+    console.log(result);
+    var colors = result['colors']['hex'];
+     console.log(colors[0]);
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
 }
